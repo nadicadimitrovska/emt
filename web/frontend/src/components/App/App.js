f8e31c9612e99b59697057bc9ef847e4383e1279
Your Name <you@example.com>
2023-03-26 20:00:17 +0200

    Add React part2

--- web/frontend/src/components/App/App.js
+++ web/frontend/src/components/App/App.js
@@ -6,6 +6,8 @@ import EShopService from "../../repository/eshopRepository";
 import Categories from "../Categories/categories";
 import Products from "../Products/ProductList/products";
 import Header from "../Header/header";
+import ProductAdd from "../Products/ProductAdd/productAdd";
+import ProductEdit from "../Products/ProductEdit/productEdit";
 
 class App extends Component {
     constructor(props) {
@@ -13,7 +15,8 @@ class App extends Component {
         this.state = {
             manufacturers: [],
             products: [],
-            categories: []
+            categories: [],
+            selectedProduct: {}
         }
     }
 
@@ -25,7 +28,9 @@ class App extends Component {
                     <div className="container">
                         <Route path={"/manufacturers"} exact render={() => <Manufacturers manufacturers={this.state.manufacturers}/>}/>
                         <Route path={"/categories"} exact render={() => <Categories categories={this.state.categories}/>}/>
-                        <Route path={"/products"} exact render={() => <Products products={this.state.products}/>}/>
+                        <Route path={"/products/add"} exact render={() => <ProductAdd categories={this.state.categories} manufacturers={this.state.manufacturers} onAddProduct={this.addProduct}/>}/>
+                        <Route path={"/products/edit/:id"} exact render={() => <ProductEdit categories={this.state.categories} manufacturers={this.state.manufacturers} onEditProduct={this.editProduct} product={this.state.selectedProduct}/>}/>
+                        <Route path={"/products"} exact render={() => <Products products={this.state.products} onDelete={this.deleteProduct} onEdit={this.getProduct}/>}/>
                         <Redirect to={"/products"}/>
                     </div>
                 </main>
@@ -60,6 +65,35 @@ class App extends Component {
             });
     }
 
+    deleteProduct = (id) =>{
+        EShopService.deleteProduct(id)
+            .then(() => {
+                this.loadProducts();
+            });
+    }
+
+    addProduct = (name, price, quantity, category, manufacturer) =>{
+        EShopService.addProduct(name,price,quantity,category,manufacturer)
+            .then(()=>{
+                this.loadProducts();
+            });
+    }
+
+    getProduct = (id) => {
+        EShopService.getProduct(id)
+            .then((data) =>{
+               this.setState({
+                   selectedProduct: data.data
+               })
+            });
+    }
+
+    editProduct = (id, name, price, quantity, category, manufacturer) => {
+        EShopService.editProduct(id,name,price,quantity,category,manufacturer)
+            .then(()=>{
+                this.loadProducts();
+            });
+    }
 
     componentDidMount() {
         this.loadManufacturers();
--- web/frontend/src/repository/eshopRepository.js
+++ web/frontend/src/repository/eshopRepository.js
@@ -9,7 +9,34 @@ const EShopService={
     },
     fetchProducts: () => {
         return axios.get("/products");
+    },
+
+    deleteProduct: (id) => {
+        return axios.delete(`/products/delete/${id}`); //products/delete/5
+    },
+    addProduct: (name, price, quantity, category, manufacturer) => {
+        return axios.post("/products/add",{
+            "name": name,
+            "price": price,
+            "quantity": quantity,
+            "category": category,
+            "manufacturer": manufacturer
+        });
+    },
+
+    editProduct: (id, name, price, quantity, category, manufacturer) => {
+        return axios.put(`/products/edit/${id}`,{
+            "name": name,
+            "price": price,
+            "quantity": quantity,
+            "category": category,
+            "manufacturer": manufacturer
+        });
+    },
+    getProduct: (id) => {
+        return axios.get(`/products/${id}`);
     }
 }
+
 
 export default EShopService;
